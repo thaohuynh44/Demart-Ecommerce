8d426cc3aeb43c942083d1e8aa49efcf65af945b
Thao Huynh <huynhngocthao989@gmail.com>
2021-09-17 20:34:21 +0700

    ShopFeature

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
+
 
 @Component({
   selector: 'app-root',
@@ -10,16 +8,9 @@ import { IProduct } from './models/product';
 })
 export class AppComponent implements OnInit{
   title = 'Demart';
-  products!: IProduct[];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.http.get<IPagination>('https://localhost:5001/api/product?pageSize=50')
-      .subscribe((response: IPagination) => {
-        this.products = response.data;
-      }, error => {
-        console.log(error);
-      });
   }
 }
